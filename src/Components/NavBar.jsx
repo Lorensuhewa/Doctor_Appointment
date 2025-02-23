956aadea6846ccadb27c49834a1d1fa7a4b0870a
Sanaka Lorensuhewa <153020684+Lorensuhewa@users.noreply.github.com>
2025-02-24 00:26:03 +0530

    create header section

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import { NavLink, useNavigate } from 'react-router-dom'
+import Profile_img from '../assets/profile_pic.png'
 
 const NavBar = () => {
 
@@ -33,7 +34,7 @@ const NavBar = () => {
         {
           token?
           <div className='flex items-center gap-2 cursor-pointer group relative'>
-            <img className='w-8 rounded-full' src="../assets/profile_pic.png" alt=' '/>
+            <img className='w-8 rounded-full' src={Profile_img} alt=' '/>
             <img className='w-2.5 rounded-full' src="" alt=' '/>
             <div className='absolute top-0 right-0 pt-14 text-base font-medium text-gray-600 z-20 hidden group-hover:block'>
               <div className='min-w-48 bg-stone-100 rounded flex flex-col gap-4 p-4'>
