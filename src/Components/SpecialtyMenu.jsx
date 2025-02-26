a22ee5600a9736cbc3a491bb00412bf9507d8e86
Sanaka Lorensuhewa <153020684+Lorensuhewa@users.noreply.github.com>
2025-02-26 16:51:40 +0530

    add docker file

--- src/Components/SpecialtyMenu.jsx
+++ src/Components/SpecialtyMenu.jsx
@@ -6,14 +6,13 @@ function SpecialtyMenu() {
   return (
     <div id='specialty' className='flex flex-col items-center gap-4 py-16 text-gray-800 '>
         <h1 className='text-3xl font-medium'>Find by Specialty </h1>
-        <p className='sm:w-1/3 text-center text-sm'> Simply browse through our extensive list of trusted doctors, schedule your appointment hassle-free. </p>
-        <div className='flex gap-4 pt-5 sm:justify-center w-full overflow-scroll'>
+        <p className='text-sm text-center sm:w-1/3'> Simply browse through our extensive list of trusted doctors, schedule your appointment hassle-free. </p>
+        <div className='flex w-full gap-5 pt-5 overflow-scroll sm:justify-center'>
             {specialtyData.data.map((item,index) => (
-                    <Link onClick={()=> scrollTo(0,0)} className='flex flex-col items-center text-xs border border-blue-500 cursor-pointer hover:translate-y-[-10px] transition-all duration-500' key={index} to={`/doctors/${item.name}`}>
-                        <img className='w-16 sm:w-24 mb-2 ' src={item.img} alt="" />
-                        <h3>{item.name}</h3>
-                        <p className='sm:w-1/10 text-center text-sm'>{item.description}</p>
-
+                    <Link onClick={()=> scrollTo(0,0)} className='flex flex-col items-center text-xs w-[250px] border rounded-full border-blue-500 cursor-pointer hover:translate-y-[-10px] transition-all duration-500' key={index} to={`/doctors/${item.name}`}>
+                        <img className='w-16 p-1 mb-2 sm:w-24' src={item.img} alt="" />
+                        <h3 className='p-2'>{item.name}</h3>
+                        
                     </Link>
                 ))
             }
